9df896f07298c3ff37c34a43ee787e1dbc2b4a35
BaBy-github <70896306+BaBy-github@users.noreply.github.com>
2022-08-20 16:21:02 +0800

    init

---
+++ src/main/java/com/example/babyadminapi/BabyAdminApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.babyadminapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabyAdminApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BabyAdminApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/babyadminapi/BabyAdminApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.babyadminapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabyAdminApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
