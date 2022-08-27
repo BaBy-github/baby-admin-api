483d2d87e51e2d8ccf3ac2cc1409ba2f81c5071d
BaBy-github <70896306+BaBy-github@users.noreply.github.com>
2022-08-27 23:16:34 +0800

    feat: 将权限数据放在缓存

--- src/main/java/com/example/babyadminapi/config/saTokenConfig/SaCustomizePermissionAuthentication.java
+++ src/main/java/com/example/babyadminapi/config/saTokenConfig/SaCustomizePermissionAuthentication.java
@@ -1,10 +1,14 @@
 package com.example.babyadminapi.config.saTokenConfig;
 
+import cn.dev33.satoken.session.SaSession;
+import cn.dev33.satoken.session.SaSessionCustomUtil;
 import cn.dev33.satoken.stp.StpInterface;
+import cn.dev33.satoken.stp.StpUtil;
 import com.example.babyadminapi.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -18,11 +22,27 @@ public class SaCustomizePermissionAuthentication implements StpInterface {
 
     @Override
     public List<String> getPermissionList(Object loginId, String loginType) {
-        return userService.getPermissionsByUserId(Integer.parseInt(loginId.toString()));
+        // 1. 声明权限码集合
+        List<String> permissionList = new ArrayList<>();
+
+        // 2. 遍历角色列表，查询拥有的权限码
+        for (String roleId : getRoleList(loginId, loginType)) {
+            SaSession roleSession = SaSessionCustomUtil.getSessionById("role-" + roleId);
+            List<String> list = roleSession.get("Permission_List", () -> {
+                return userService.getPermissionsByUserId(Integer.parseInt(loginId.toString()));     // 从数据库查询这个角色所拥有的权限列表
+            });
+            permissionList.addAll(list);
+        }
+
+        // 3. 返回权限码集合
+        return permissionList;
     }
 
     @Override
     public List<String> getRoleList(Object loginId, String loginType) {
-        return userService.getRolesByUserId(Integer.parseInt(loginId.toString()));
+        SaSession session = StpUtil.getSessionByLoginId(loginId);
+        return session.get("Role_List", () -> {
+            return userService.getRolesByUserId(Integer.parseInt(loginId.toString())); // 从数据库查询这个账号id拥有的角色列表
+        });
     }
 }
